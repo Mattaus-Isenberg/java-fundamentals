d9c77074cfcc7f7a813ec29bd284aff3387cfe45
Mattäus Sebastian Isenberg <mattaus@gmx.com>
2020-01-06 20:21:39 -0800

    Initial Setup

---
+++ basics/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void public static void main(String[] args) {
+
+    }
+
+    public static String reverseArray(String word, int number) {
+        String outString = new String("Temp");
+        return outString;
+    }
+
+    public static void flipNHeads(int numberToFlip) {
+
+    }
+
+    public static void clock() {
+
+    }
+
+}
