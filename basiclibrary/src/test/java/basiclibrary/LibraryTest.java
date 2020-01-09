5b1b68d61dcda85cbff070c3cbad7d1d39f553b7
Mattäus Sebastian Isenberg <mattaus@gmx.com>
2020-01-09 13:25:57 -0800

    Tasks 1 and 2 completed

--- basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -50,4 +50,29 @@ public class LibraryTest {
       int[] returnArray = {55, 54, 60, 53, 59, 57, 61};
       assertArrayEquals(returnArray, Library.arrayOfArrays(weeklyMonthTemperatures));
   }
+
+    @Test public void testWeatherAnalysis(){
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+        assertEquals("High: 72\nLow: 51\nNever saw temperature: 63\nNever saw temperature: 67\nNever saw temperature: 68\nNever saw temperature: 69\n", Library.weatherAnalysis(weeklyMonthTemperatures));
+    }
+
+    @Test public void testTally() {
+        ArrayList<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+        assertEquals("Bush received the most votes!", Library.tally(votes));
+    }
 }
